38372cbabe1dcbc771055c2c187e0dd7c1c5de49
widyaput <13519105@std.stei.itb.ac.id>
2022-05-29 17:59:14 +0700

    change contact

--- src/data/request.ts
+++ src/data/request.ts
@@ -169,4 +169,4 @@ export const notices = [
   'Proyek yang sudah memiliki rancangan tampilan & desain akan dapat diproses dengan lebih cepat.',
 ];
 
-export const contacts = ['082119503294'];
+export const contacts = ['0895420592600'];
